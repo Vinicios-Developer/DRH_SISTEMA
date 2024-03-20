aa9aed01b2568a752a312f886b795f315cf1aca0
Vinicios-Developer <viniciosgabriel_18@hotmail.com>
2024-03-20 13:44:06 -0400

    página de login (apenas o html)

--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,16 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 from flask_ckeditor import CKEditor
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ''
+# app.config['SQLALCHEMY_DATABASE_URI'] = ''
 app.config['CKEDITOR_PKG_TYPE'] = 'full'
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '8d128903f0d3bcec0f4866d4444951f02b1f22c891dafe2f'
 
-database = SQLAlchemy(app)
+# database = SQLAlchemy(app)
 ckeditor = CKEditor(app)
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'home'
-login_manager.login_message_category = 'alert-info'
+
+from cerebro.src.routes import routes
